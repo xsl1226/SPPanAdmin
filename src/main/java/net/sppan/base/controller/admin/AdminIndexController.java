083cae592de60bfe827f84588aab22acb340664d
DELL <DELL@AACN-R002.centaline.com.cn>
2018-03-27 10:43:58 +0800

    Initial commit

---
+++ src/main/java/net/sppan/base/controller/admin/AdminIndexController.java
@@ -0,0 +1,15 @@
+package net.sppan.base.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.sppan.base.controller.BaseController;
+
+@Controller
+public class AdminIndexController extends BaseController {
+    @RequestMapping(value = {"/admin/", "/admin/index"})
+    public String index() {
+
+        return "admin/index";
+    }
+}
---
+++ src/main/java/net/sppan/base/service/IResourceService.java
@@ -0,0 +1,45 @@
+package net.sppan.base.service;
+
+import net.sppan.base.entity.Resource;
+import net.sppan.base.service.support.IBaseService;
+import net.sppan.base.vo.ZtreeView;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 资源服务类
+ * </p>
+ *
+ * @author SPPan
+ * @since 2016-12-28
+ */
+public interface IResourceService extends IBaseService<Resource, Integer> {
+
+    /**
+     * 获取角色的权限树
+     *
+     * @param roleId
+     * @return
+     */
+    List<ZtreeView> tree(int roleId);
+
+    /**
+     * 修改或者新增资源
+     *
+     * @param resource
+     */
+    void saveOrUpdate(Resource resource);
+
+    /**
+     * 关键字分页
+     *
+     * @param searchText
+     * @param pageRequest
+     * @return
+     */
+    Page<Resource> findAllByLike(String searchText, PageRequest pageRequest);
+
+}
